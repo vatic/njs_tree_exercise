3186d2de9f508fdc9313f2b07a9a85cb2d0444ab
Ruslan Shelekhov <cto@mlm-soft.com>
2020-11-22 02:30:46 +0300

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const Tree = require('./tree_data_mock.js');
+
+function incValue(object_id, value_increment) {
+    // TODO. Code the actual excercise here, please...
+}
+
+// Test cases
+incValue( 303,   303);
+incValue(1701, -1701);
+incValue(9000,  9000);
+
+// Print-out the result
+console.log(JSON.stringify(Tree, null, "\t"));
---
+++ tree_data_mock.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        // Just an object id
+        id: 1,
+        // Id of parent/ascendant object
+        parent_id: 9000,
+        // Number we are actually calculating. If not exists = 0
+        value: 0,
+    },
+    {
+        id: 2,
+        parent_id: 1,
+        value: 22,
+    },
+    {
+        id: 3,
+        parent_id: 2,
+        value: -3,
+    },
+    {
+        id: 4,
+        parent_id: 2,
+        value: 404.04,
+    },
+    {
+        id: 303,
+        parent_id: 3,
+    },
+    {
+        iD: 404,
+    },
+    {
+        id: 505,
+        parent_id: 5,
+        value: .055,
+    },
+    {
+        id: 1701,
+        value: 100500,
+    },
+    {
+        // This is the parent if no 'parent_id' defined OR object with 'parent_id' is not exists. Only one 'default_parent' per tree.
+        default_parent: true,
+        id: 9000,
+    },
+];
